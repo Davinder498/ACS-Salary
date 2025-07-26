236dd678129d83926d991a3e276139a571d69a5e
Davinder498 <davinder.silver@gmail.com>
2025-07-26 03:49:16 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',  // <--- Ensure this is added
+  build: {
+    outDir: 'dist', // Netlify expects the build output here
+  },
 });
