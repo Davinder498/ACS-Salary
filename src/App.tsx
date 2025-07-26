9bc98fbf3d5d25fdcf1687a7979b22c132adb10a
Davinder498 <davinder.silver@gmail.com>
2025-07-26 03:47:33 -0600

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>ACS Salary Calculator</h1>
+      <p>The app is working!</p>
+    </div>
+  )
+}
+
+export default App
