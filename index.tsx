e985d059b070015267be35e9516284139e81372d
Davinder498 <davinder.silver@gmail.com>
2025-08-05 16:59:49 -0600

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2636,5 +2636,3 @@ const App = () => {
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
